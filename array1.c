2fdd4499bec23e5aac142bb884770746b38d8bbb
aditya <adityanavadiya6790@gmail.com>
2025-09-08 14:51:09 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a[4],i;
+printf("enter five value");
+for(i=0;i<=4;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=4;i++)
+{
+    printf("five values are:%d\n",a[i]);
+}
+}
+
---
+++ array2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a[9],i;
+printf("enter five value");
+for(i=0;i<=9;i++)
+{
+scanf("%d",&a[i]);
+}
+    printf("fourth value is:%d\n",a[3]);
+    printf("seventh value is:%d\n",a[6]);
+    printf("nineth value is:%d\n",a[8]);
+
+}
+
